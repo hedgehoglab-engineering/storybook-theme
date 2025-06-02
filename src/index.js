bb987a0251b53e33f5ec734d51a36bd04d7d5c6b
Sam Turrell <sam.turrell@hedgehoglab.com>
2025-06-02 15:10:18 +0100

    use preview-api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { create, color, typography } from 'storybook/theming';
-import { addons } from '@storybook/addons';
+import { addons } from 'storybook/preview-api';
 import brandImage from './logo.svg';
 
 const theme = create({
